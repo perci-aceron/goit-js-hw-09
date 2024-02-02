6ef647942a7b26eaff05dbaa5408f90842808ab9
Percival Aceron <perci.aceron@gmail.com>
2024-02-02 14:48:06 +0800

    revision

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
 
 let intervalId;
 
 function start() {
+  if (intervalId) {
+    return;
+  }
+
   document.querySelector('[data-start]').disabled = true;
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
@@ -16,8 +20,11 @@ function start() {
 function stop() {
   clearInterval(intervalId);
   document.querySelector('[data-start]').disabled = false;
+  intervalId = null;
 }
 
-document.querySelector('[data-start]').addEventListener('click', start);
-document.querySelector('[data-stop]').addEventListener('click', stop);
+  document.querySelector('[data-start]').addEventListener('click', start);
+  document.querySelector('[data-stop]').addEventListener('click', stop);
 
+  start();
+  
