173c9cab00d2f00af5fa76073e4a47e24df4519b
Percival Aceron <perci.aceron@gmail.com>
2024-02-02 14:34:15 +0800

    Updates

---
+++ 01-color-switcher.1d7dbe11.js
@@ -0,0 +1,2 @@
+!function(){var t;document.querySelector("[data-start]").addEventListener("click",(function(){document.querySelector("[data-start]").disabled=!0,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(t),document.querySelector("[data-start]").disabled=!1})),document.querySelector("[data-start]").click()}();
+//# sourceMappingURL=01-color-switcher.1d7dbe11.js.map
