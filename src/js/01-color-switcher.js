e04c7d84e2bb5b431e4dc2020730cedc8426c890
Percival Aceron <perci.aceron@gmail.com>
2024-02-02 14:08:10 +0800

    Initial commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let intervalId;
+
+function start() {
+  document.querySelector('[data-start]').disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(intervalId);
+  document.querySelector('[data-start]').disabled = false;
+}
+
+document.querySelector('[data-start]').addEventListener('click', start);
+document.querySelector('[data-stop]').addEventListener('click', stop);
+document.querySelector('[data-start]').click();
